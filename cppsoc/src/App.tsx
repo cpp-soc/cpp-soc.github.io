7cc8b21845e399ea36de31efde34cd75ea25f0be
tommy phao <thomasphao04@gmail.com>
2024-04-17 19:23:01 -0700

    test webhook 2

--- cppsoc/src/App.tsx
+++ cppsoc/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Cal Poly - Security Operations Center
+          Welcome to Cal Poly SOC
         </p>
         <a
           className="App-link"
