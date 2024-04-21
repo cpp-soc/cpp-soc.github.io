d577eee4194fed2b26af681ff2e571b7606267e2
tommy phao <thomasphao04@gmail.com>
2024-04-21 13:04:50 -0700

    Revert "test webhook 2"
    This reverts commit 7cc8b21845e399ea36de31efde34cd75ea25f0be.

--- cppsoc/src/App.tsx
+++ cppsoc/src/App.tsx
@@ -5,11 +5,29 @@ import './Styles/main.css';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className="main">
       <NavBar/>
       <h1 className="Welcome">
         Welcome to Cal Poly Pomona's Security Operation Center
       </h1>
+=======
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Cal Poly - Security Operations Center
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> parent of 7cc8b21 (test webhook 2)
     </div>
 
   );
