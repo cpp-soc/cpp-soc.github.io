384729fc2c2222f406948ada01ce6ec95418f810
tommy phao <thomasphao04@gmail.com>
2024-04-21 12:06:04 -0700

    Creating new pags

--- cppsoc/src/App.tsx
+++ cppsoc/src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import NavBar from './Components/navbar';
+import './Styles/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Cal Poly SOC
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <NavBar/>
+      <h1 className="Welcome">
+        Welcome to Cal Poly Pomona's Security Operation Center
+      </h1>
     </div>
+
   );
 }
 
---
+++ cppsoc/src/Components/navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+    return (
+      <nav>
+        <Link to={"./"}>Home </Link>
+        <Link to={"./Team"}>Team </Link>
+        <Link to={"./FAQ"}>FAQ </Link>
+        <Link to={"./Sponsors"}>Contact Us! </Link>
+      </nav>
+    )
+  }
+  
+export default NavBar
