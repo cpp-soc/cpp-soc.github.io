696fb14158c194e38d269a46b5cc464be2ad3a24
tommy phao <thomasphao04@gmail.com>
2024-04-22 17:41:13 -0700

    Pushed to own development branch Updated Home page text with what Joe provided.

--- cppsoc/src/Pages/Home.jsx
+++ cppsoc/src/Pages/Home.jsx
@@ -1,11 +1,29 @@
 function Home() {
   return (
-    <div className="container">
+    <>
+        <div className="container-welcome">
       <div className="welcome">
-        <h1>Cal Poly Pomona - SOC</h1>
-        <h3>A student-led Security Operations Center</h3>
+        <h1>
+          Welcome to the Student Security Operations Center (SOC) @ Cal Poly
+          Pomona
+        </h1>
+
+        <h3>
+          Our mission is to monitor the Student Data Center (SDC) and improve
+          its security posture by continuously monitoring logs using System
+          Information and Event Monitoring technologies like Splunk, IBM QRadar
+          and Microsoft Sentinel.
+        </h3>
+
+        <h3>
+          Our goal is also to provide students interested in Cybersecurity the
+          relevant skills that they will need to give them a head start on their
+          Cybersecurity careers.
+        </h3>
+
       </div>
     </div>
+    </>
   );
 }
 
