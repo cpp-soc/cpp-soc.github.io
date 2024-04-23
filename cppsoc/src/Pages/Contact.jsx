e4e8e95b56e12c6c710b06246330bdf9404b6ef4
xdkaine <55013938+xdkaine@users.noreply.github.com>
2024-04-22 21:16:00 -0700

    Updated Projects to handle a bulleted bio system Updated Contact Media (basic) Added CSS

--- cppsoc/src/Pages/Contact.jsx
+++ cppsoc/src/Pages/Contact.jsx
@@ -1,13 +1,31 @@
 function Contact() {
-    return (
-      <div className="container">
-        <div className="welcome">
-          <h1>Contact!</h1>
-          <h3>Or someting!</h3>
+  return (
+    <div className="container">
+      <div className="welcome">
+        <p>
+          Feel free to reach out to us through Discord or explore more links on
+          our Linktree:
+        </p>
+        <div className="media">
+          <iframe
+            src="https://discord.com/widget?id=1222272548306292827&theme=dark"
+            width="500"
+            height="500"
+            allowtransparency="true"
+            frameborder="0"
+            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+          ></iframe>
+          <a href="https://linktr.ee/cppsoc">
+            <img
+              src="https://assets.production.linktr.ee/profiles/_next/static/logo-assets/default-meta-image.png"
+              width="500"
+              height="250"
+            ></img>
+          </a>
         </div>
       </div>
-    );
-  }
-  
-  export default Contact;
-  
+    </div>
+  );
+}
+
+export default Contact;
