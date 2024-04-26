0b590ff1363f4e9642caf8a1a7491293b243cfd2
tommy phao <thomasphao04@gmail.com>
2024-04-25 17:27:01 -0700

    Updated navbar to contain img src from github

--- cppsoc/src/Components/navbar.jsx
+++ cppsoc/src/Components/navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 function NavBar() {
   return (
     <ul>
-      <img className="navbarlogo" src="../src/Assets/2024/Media/SOC.png"/>
+      <img className="navbarlogo" src="https://github.com/cpp-soc/cpp-soc.github.io/blob/main/cppsoc/src/Assets/2024/Media/SOC.png?raw=true"/>
       <li><NavLink to={"./"} activeClassName="active">Home </NavLink></li>
       <li><NavLink to={"./Projects"} activeClassName="active">Projects </NavLink></li>
       <li><NavLink to={"./Team"} activeClassName="active">Team </NavLink></li>
