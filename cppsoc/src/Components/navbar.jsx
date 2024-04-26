24b35b112941d1b9e0c3a791365e4aff99d83a7e
tommy phao <thomasphao04@gmail.com>
2024-04-25 17:25:50 -0700

    Updated Navbar Design

--- cppsoc/src/Components/navbar.jsx
+++ cppsoc/src/Components/navbar.jsx
@@ -2,14 +2,15 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav>
-      <NavLink to={"./"} activeClassName="active">Home </NavLink>
-      <NavLink to={"./Projects"} activeClassName="active">Projects </NavLink>
-      <NavLink to={"./Team"} activeClassName="active">Team </NavLink>
-      <NavLink to={"./FAQ"} activeClassName="active">FAQ </NavLink>
-      <NavLink to={"./Contact"} activeClassName="active">Contact </NavLink>
+    <ul>
+      <img className="navbarlogo" src="../src/Assets/2024/Media/SOC.png"/>
+      <li><NavLink to={"./"} activeClassName="active">Home </NavLink></li>
+      <li><NavLink to={"./Projects"} activeClassName="active">Projects </NavLink></li>
+      <li><NavLink to={"./Team"} activeClassName="active">Team </NavLink></li>
+      <li><NavLink to={"./FAQ"} activeClassName="active">FAQ </NavLink></li>
+      <li><NavLink to={"./Contact"} activeClassName="active">Contact </NavLink></li>
       {/* Add more NavLink components for other navigation items */}
-    </nav>
+    </ul>
   );
 }
 
